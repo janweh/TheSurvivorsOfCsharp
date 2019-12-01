84f6da02f7ae025c1afa04e47a3a462265e449e4
PandaPandaa <50715542+PandaPandaa@users.noreply.github.com>
2019-12-01 22:03:13 +0200

    Implemented search by uni and search by course name

--- TheSurvivorsOfCsharp/Pages/Courses/Index.cshtml.cs
+++ TheSurvivorsOfCsharp/Pages/Courses/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TheSurvivorsOfCsharp.Data;
 using TheSurvivorsOfCsharp.Model;
@@ -20,10 +21,36 @@ namespace TheSurvivorsOfCsharp.Pages.Courses
         }
 
         public IList<Course> Course { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Universities { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string University { get; set; }
+
 
         public async Task OnGetAsync()
         {
-            Course = await _context.Course
+            IQueryable<string> UniQuery = from u in _context.University
+                                            orderby u.Name
+                                          select u.Name;
+
+      
+
+            var courses = from c in _context.Course
+                         select c;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                courses = courses.Where(s => s.Name.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(University))
+            {
+                courses = courses.Where(x => x.University.Name == University);
+            }
+
+
+            Universities = new SelectList(await UniQuery.Distinct().ToListAsync());
+            Course = await courses
                 .Include(c => c.University).ToListAsync();
         }
     }
